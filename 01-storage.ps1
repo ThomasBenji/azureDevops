d9ead4be4906a18feec0586b633b2734e8bdf9c1
thomasbenji <thomasbenjamin9095@gmail.com>
2023-11-03 22:41:20 -0400

    initial commit

--- 01-storage.ps1
+++
@@ -1,7 +0,0 @@
-$rg = 'arm-storage-01'
-New-AzResourceGroup -Name $rg -Location northeurope -Force
-
-New-AzResourceGroupDeployment `
-    -Name 'new-storage_deploy' `
-    -ResourceGroup $rg `
-    -TemplateFile '01-storage.json'
